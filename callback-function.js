ec5ffaaa85cd31bf2f40e9c63fb45c13faf9af4a
Joyanta Mondal <56169323+w3codelabs@users.noreply.github.com>
2021-08-30 17:57:48 +0600

    call back function added

---
+++ callback-function.js
@@ -0,0 +1,29 @@
+function welcomeMessage (name, greetHandler){
+    console.log(greetHandler);
+    greetHandler(name);
+}
+
+/* 
+// Array sending to the function
+const names = ['Tom Hero','Tom Shamol','Tom Samsu'] 
+welcomeMessage(names);
+*/
+
+/* 
+// object send to the function
+const nameObj ={name:'Pori Mala',age:16, profession: 'singer'};
+welcomeMessage(nameObj); 
+*/
+
+// function pass into another function
+function greetMorning(name){
+    console.log('Good Morning',name);
+}
+welcomeMessage('Hero',greetMorning);
+welcomeMessage('Hero Pro',greetMorning);
+// function pass into another function
+function greetAfternoon(name){
+    console.log('Good Afternoon',name);
+}
+welcomeMessage('Hero',greetAfternoon);
+
