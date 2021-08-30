e14b871a9b6198480a9bfc84cc6ecbfbb9a383ca
Joyanta Mondal <56169323+w3codelabs@users.noreply.github.com>
2021-08-30 16:26:04 +0600

    scope.js added

---
+++ double-triple.js
@@ -0,0 +1,19 @@
+// tripple ==== equal is best for comparisom
+const first =2;
+let second = '2';
+if(first === second){
+    console.log('True This Condition')
+}
+else{
+    console.log('False This Condition')
+
+}
+// more comparison
+const a= {name:'j'};
+const b= {name:'j'};
+if(a==b){
+console.log('trueeeeeeee');
+}
+else(
+    console.log('falseeeeeee')
+)
